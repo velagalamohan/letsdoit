eeb2abd549a2b03b003005ea7d35366d32867e47
velagalamohan <velagalamohan@gmail.com>
2023-11-13 15:30:46 +0530

    my task done

---
+++ src/com/git/SampleGithub.java
@@ -0,0 +1,19 @@
+package com.git;
+
+public class SampleGithub {
+	public void stdUsername() {
+		System.out.println("velagalamohan");
+
+	}
+	public void stdUserEmail() {
+       System.out.println("velagalamohan@gmail.com");
+       
+	}
+	public static void main(String[] args) {
+		SampleGithub gb = new SampleGithub();
+		gb.stdUserEmail();
+		gb.stdUsername();
+		
+	}
+
+}
