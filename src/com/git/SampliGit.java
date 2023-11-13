9c2845b439a0b317bc2901dc1ce269443d795dc3
velagalamohan <velagalamohan@gmail.com>
2023-11-13 15:02:29 +0530

    first

---
+++ src/com/git/SampliGit.java
@@ -0,0 +1,20 @@
+package com.git;
+
+public class SampliGit {
+	public void empName() {
+		System.out.println("Mohan");
+
+	}
+	public void empId() {
+		System.out.println("32428");
+
+	}
+
+	public static void main(String[] args) {
+		SampliGit g = new SampliGit();
+		g.empName();
+		g.empId();
+
+	}
+
+}
