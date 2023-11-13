81f7dae6fe76ac8a2576eac2776d7d0ec99967b3
velagalamohan <96651848+velagalamohan@users.noreply.github.com>
2023-11-13 15:49:55 +0530

    Update SampleGithub.java

--- src/com/git/SampleGithub.java
+++ src/com/git/SampleGithub.java
@@ -2,7 +2,7 @@ package com.git;
 
 public class SampleGithub {
 	public void stdUsername() {
-		System.out.println("velagalamohan")
+		System.out.println("velagalamohan");
 	}
 	public void stdUserEmail() {
        System.out.println("mohanaraovelagala@gmail.com");
